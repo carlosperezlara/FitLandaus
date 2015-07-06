3c879be9fbf548a66ce43fc79dd83a1c6cd53cae
Carlos Perez <cperez@adm-129-49-192-67.wi-fi.stonybrook.edu>
2015-07-06 17:10:47 -0400

    toCondor

--- fitjob.sh
+++ fitjob.sh
@@ -1,9 +1,16 @@
 RUN=434017_435939
 DRAW=1
-ARM=$1
-LYR=$2
-SEN=$3
-ROW=$4
+IDX=$1
 
-root -b -l -q scripts/fitrow.C\(\"${RUN}\",${ARM},${LYR},${SEN},${MPD},${DRAW},6.5,82.5,50\)
-root -b -l -q scripts/fitrow.C\(\"${RUN}\",${ARM},${LYR},${SEN},${MPD},${DRAW},6.5,82.5,50\)
+
+let "ARM=${IDX}/768"
+let "ONEARM=${IDX}%768"
+let "LYR=${ONEARM}/96"
+let "ONELYR=${ONEARM}%96"
+let "SEN=${ONELYR}/4"
+let "ROW=${ONELYR}%4"
+
+echo "******** I AM WORKING ON:  ARM ${ARM} | LYR ${LYR} | SEN ${SEN} | ROW ${ROW}"
+
+#root -b -l -q scripts/fitrow.C\(\"${RUN}\",${ARM},${LYR},${SEN},${MPD},${DRAW},6.5,82.5,50\)
+#root -b -l -q scripts/fitrow.C\(\"${RUN}\",${ARM},${LYR},${SEN},${MPD},${DRAW},6.5,82.5,50\)
